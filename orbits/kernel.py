f5f94e128b032143fd25841d9cd063e55d1f0c75
Thomas Vandal <thomasvandal@hotmail.com>
2021-08-24 18:59:42 -0400

    Document kernel-creation functions

--- orbits/kernel.py
+++ orbits/kernel.py
@@ -1,11 +1,28 @@
 from typing import Union
 
+import aesara_theano_fallback.tensor as tt
 import pymc3 as pm
 from celerite2.theano import terms
 from pymc3.gp.cov import Covariance
 
 
-def get_qp_kernel(period, ls_exp, ls_per) -> Covariance:
+def get_qp_kernel(
+    period: Union[tt.TensorVariable, float],
+    ls_per: Union[tt.TensorVariable, float],
+    ls_exp: Union[tt.TensorVariable, float],
+) -> Covariance:
+    """
+    Helper function to create a quasi-periodic covariance function in PyMC3.
+
+    :param period: Period of the periodic component
+    :type period: Union[tt.TensorVariable, float]
+    :param ls_per: Length scale of the periodic component
+    :type ls_per: Union[tt.TensorVariable, float]
+    :param ls_exp: Length scale of the squared exponential component
+    :type ls_exp: Union[tt.TensorVariable, float]
+    :return: Quasi-periodic convariance function
+    :rtype: Covariance
+    """
 
     sqexp_kernel = pm.gp.cov.ExpQuad(1, ls=ls_exp)
     per_kernel = pm.gp.cov.Periodic(1, period, ls=ls_per)
@@ -40,19 +57,22 @@ KERNEL_PARAMS = {
 
 
 def get_pm3_kernel(
-    kernel_name: str, sigma: Union[pm.Distribution, float], **kwargs
+    kernel_name: str, sigma: Union[tt.TensorVariable, float], **kwargs
 ) -> Covariance:
     """
     Function that creates a PyMC3 kernel by adding an amplitude to the built-in
     covraiance functions.
 
-    NOTE: This does not handle celerite terms/kernels.
+    All kwargs are passed to the PyMC3 covariance function directy.
+
+    NOTE: This does not handle celerite2 terms/kernels. As they usually
+    include sigma and are built for 1d timeseries.
 
     :param kernel_name: Kernel name (name of the class, but as a string)
     :type kernel_name: str
     :param sigma: Amplitude of the GP (pymc3 cov is multiplied by sigma^2).
                   NOTE: Not 100% sure about the type below.
-    :type sigma: Union[pm.Distribution, float]
+    :type sigma: Union[tt.TensorVariable, float]
     :return: PyMC3 covariance function that can be used in regular pymc3 gp.
     :rtype: Covariance
     """
