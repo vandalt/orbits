cd897c02d79826d6663b31de203e6d3e12ce21c8
Thomas Vandal <thomasvandal@hotmail.com>
2021-08-31 14:34:02 -0400

    Add support to intialize model without param dict

--- orbits/model.py
+++ orbits/model.py
@@ -1,4 +1,5 @@
-from typing import Optional
+import warnings
+from typing import Optional, Union
 
 import aesara_theano_fallback.tensor as tt
 import exoplanet as xo
@@ -60,7 +61,9 @@ class PlanetModel(Model):
         These parameters will be used for orbit calculations.
         """
 
-        return get_synth_params(self.named_vars, prefix=self.name)
+        # If this is called from a parent model, we need to change context
+        with self:
+            return get_synth_params(self.named_vars, prefix=self.name)
 
 
 class GPModel(Model):
@@ -106,7 +109,8 @@ class GPModel(Model):
         if self._kernel is not None:
             return self._kernel
         else:
-            self._kernel = KERNELS[self.kernel_name](**self.gp_dict)
+            with self:
+                self._kernel = KERNELS[self.kernel_name](**self.gp_dict)
             return self._kernel
 
     @property
@@ -123,20 +127,22 @@ class GPModel(Model):
         parameters for a given kernel are there.
         """
 
-        gp_dict = dict()
-        nvars = self.named_vars
-        for pname in KERNEL_PARAMS[self.kernel_name]:
-            if f"{self.prefix}{pname}" in nvars:
-                gp_dict[pname] = nvars[f"{self.prefix}{pname}"]
-            elif f"{self.prefix}log{pname}" in nvars:
-                gp_dict[pname] = pm.Deterministic(
-                    pname, tt.exp(nvars[f"{self.prefix}log{pname}"])
-                )
-            else:
-                raise ValueError(
-                    f"Kernel {self.kernel_name} requires parameter "
-                    f"{pname} or log{pname}"
-                )
+        # If called from parent, need to switch context
+        with self:
+            gp_dict = dict()
+            nvars = self.named_vars
+            for pname in KERNEL_PARAMS[self.kernel_name]:
+                if f"{self.prefix}{pname}" in nvars:
+                    gp_dict[pname] = nvars[f"{self.prefix}{pname}"]
+                elif f"{self.prefix}log{pname}" in nvars:
+                    gp_dict[pname] = pm.Deterministic(
+                        pname, tt.exp(nvars[f"{self.prefix}log{pname}"])
+                    )
+                else:
+                    raise ValueError(
+                        f"Kernel {self.kernel_name} requires parameter "
+                        f"{pname} or log{pname}"
+                    )
 
         return gp_dict
 
@@ -147,10 +153,11 @@ class RVModel(Model):
         t: np.ndarray,
         vrad: np.ndarray,
         svrad: np.ndarray,
-        params: dict[str, dict],
         num_planets: int,
+        params: Optional[dict[str, dict]] = None,
         t_ref: Optional[float] = None,
         gp_kernel: Optional[str] = None,
+        planet_names: Optional[Union[str, list[str]]] = None,
         quiet_celerite: bool = False,
         name: str = "",
         model: Optional[Model] = None,
@@ -168,14 +175,18 @@ class RVModel(Model):
                        System-wide parameters should be in the "system" key.
                        GP parameters should be under the "gp" key.
                        All other keys are treated as planets (this may change
-                       to avoid ambiguities in the future)
-        :type params: dict[str, dict]
+                       to avoid ambiguities in the future), defaults to None
+        :type params: Optional[dict[str, dict]], optional
         :param num_planets: Number of planets to model in the dataset.
         :type num_planets: int
         :param t_ref: Reference time for RV trend parameters, defaults to None
         :type t_ref: Optional[float], optional
         :param gp_kernel: Name of the GP kernel, defaults to None
         :type gp_kernel: Optional[str], optional
+        :param planet_names: Name of the planets, usually a single letter.
+                             By default, the model uses lowercase letters
+                             starting with b.
+        :type planet_names: Optional[Union[str, list[str]]] , optional
         :param quiet_celerite: [TODO:description], defaults to False
         :type quiet_celerite: bool, optional
         :param name: PyMC3 model name that will prefix all variables,
@@ -205,62 +216,63 @@ class RVModel(Model):
         except (ValueError, TypeError):
             raise TypeError("num_planets should be an integer.")
 
-        # Load parameters that affect the whole system.
-        load_params(params["system"])
+        if planet_names is not None:
+            if isinstance(planet_names, str):
+                planet_names = [planet_names]
+            if len(planet_names) != num_planets:
+                raise ValueError("Need one planet name per modelled planet")
+            self.planet_names = planet_names
+        else:
+            self.planet_names = [chr(n + 98) for n in range(num_planets)]
 
+        # If there are parameters, we load RV params first
+        if params is not None:
+            # Load parameters that affect the whole system.
+            # NOTE: might not be hardcoded in future with multi-instrument
+            if "rv" in params:
+                load_params(params["rv"])
+            self._rv_params = self._get_rv_params()
+        # If no RV params, we set them to none
+        else:
+            self._rv_params = None
+
+        # Always create pl models at init, but pass params only if available
         self.planets = dict()
-        for prefix in params:
+        for prefix in self.planet_names:
 
-            # NOTE: In the future it might be better to replace this by
-            # iterating over planets explicitely. Two special cases is
-            # easy to manage for now.
-            if prefix in ["system", "gp"]:
-                # We skip non-planet entries in the parameter dictionary
-                continue
+            # NOTE: This means users can give params for one planet but skip
+            # other and give its params later... Not sure we want this but
+            # let's keep it for now
+            if params is None:
+                pl_params = None
+            elif prefix in params:
+                pl_params = params[prefix]
+            else:
+                pl_params = None
 
-            # For each planet, we load parameters
-            # Using submodel makes "{letter}_" prefix auto
             self.planets[prefix] = PlanetModel(
-                params[prefix], name=prefix, model=self
+                pl_params, name=prefix, model=self
             )
 
-        # We also make sure that parameters specific to the RV model are
-        # available (e.g. reparamtrezie log{param})
-        self._get_rv_params()
+        # Always set to none, will be created on request
+        self._synth_dict = None
 
         if len(self.planets) > 0:
-            # NOTE: will move to more generic model to support all timeseries.
-            # We access keys explicitely because the naming convention is
-            # different between RadVel and exoplanet.
-            # To use **, would need to "translate" radvel names to exoplanet or
-            # just use exoplanet names by default.
-
-            # exoplanet orbit objects require a certain parmeterization.
-            # We built vectors over planets in this "synth" parameterization
-            # (used to synthesize RVs, following RadVel nomenclature).
-            self._get_synth_params()
-
-            self.orbit = xo.orbits.KeplerianOrbit(
-                period=self.synth_dict["per"],
-                t_periastron=self.synth_dict["tp"],
-                ecc=self.synth_dict["e"],
-                omega=self.synth_dict["w"],
-            )
-
-        # Once we have our parameters, we define the RV model at data points
-        self.get_rv_model(self.t)
-        self.resid = self.vrad - self.rv_model
-
-        # Then, with the rv_model, we define a likelihood with the RV data
-        self.err = tt.sqrt(self.svrad ** 2 + self.wn ** 2)
+            # Always set to none first, will be created on request
+            # (planets might have their parameters defined later)
+            self._orbit = None
 
         # Define data likelihood depending on GP type
-        if gp_kernel is None:
-            pm.Normal("obs", mu=self.rv_model, sd=self.err, observed=self.vrad)
-        else:
-            self.gpmodel = GPModel(
-                params["gp"], gp_kernel, name="gp", model=self
-            )
+        if gp_kernel is not None:
+            if params is None:
+                gp_params = None
+            elif "gp" in params:
+                gp_params = params["gp"]
+            else:
+                gp_params = None
+
+            self.gpmodel = GPModel(gp_params, gp_kernel, name="gp", model=self)
+
             if gp_kernel in CELERITE_KERNELS:
                 self.gp = GaussianProcess(
                     self.gpmodel.kernel,
@@ -268,9 +280,93 @@ class RVModel(Model):
                     yerr=self.err,
                     quiet=quiet_celerite,
                 )
-                self.gp.marginal("obs", observed=self.resid)
             elif gp_kernel in PYMC3_KERNELS:
                 self.gp = pm.gp.Marginal(cov_func=self.gpmodel.kernel)
+            else:
+                raise ValueError(
+                    f"gp_kernel must be None, or one of {KERNEL_LIST}"
+                )
+        else:
+            self.gpmodel = None
+            self.gp = None
+
+        if params is not None:
+            try:
+                self.add_likelihood()
+            except (KeyError, AttributeError):
+                msg = (
+                    "IMPORTANT: Tried to define 'obs' likelihood, "
+                    "but some parameters are missing. Either add parameters "
+                    "to the input dictionary or define them in the model "
+                    "context. Then run model.add_likelihood().\n"
+                )
+                warnings.warn(msg, RuntimeWarning)
+
+    @property
+    def rv_params(self):
+        if self._rv_params is not None:
+            return self._rv_params
+        else:
+            self._rv_params = self._get_rv_params()
+            return self._rv_params
+
+    @property
+    def orbit(self):
+        if self._orbit is not None:
+            return self._orbit
+        elif self._orbit is None and len(self.planets) > 0:
+            self._orbit = self._get_orbit()
+            return self._orbit
+        else:
+            return None
+
+    @property
+    def synth_dict(self):
+        if self._synth_dict is not None:
+            return self._synth_dict
+        else:
+            self._synth_dict = self._get_synth_dict()
+            return self._synth_dict
+
+    @property
+    def rvmod(self):
+        if "rv_model" not in self.named_vars:
+            with self:
+                self.calc_rv_model(self.t)
+        return self.rv_model
+
+    @property
+    def resid(self):
+        return self.vrad - self.rvmod
+
+    @property
+    def err(self):
+        return tt.sqrt(self.svrad ** 2 + self.wn ** 2)
+
+    def _get_synth_dict(self):
+
+        # exoplanet orbit objects require a certain parmeterization.
+        # We build vectors over planets in this "synth" parameterization
+        # (used to synthesize RVs, following RadVel nomenclature).
+
+        synth_dict = dict()
+        for pname in SYNTH_PARAMS:
+            synth_dict[pname] = pm.Deterministic(
+                pname,
+                tt.as_tensor_variable(
+                    [pl.synth_params[pname] for pl in self.planets.values()]
+                ),
+            )
+
+        return synth_dict
+
+    def add_likelihood(self):
+        if self.gpmodel is None:
+            pm.Normal("obs", mu=self.rvmod, sd=self.err, observed=self.vrad)
+        else:
+            if self.gpmodel.kernel in CELERITE_KERNELS:
+                self.gp.marginal("obs", observed=self.resid)
+            elif self.gpmodel.kernel in PYMC3_KERNELS:
                 self.gp.marginal_likelihood(
                     "obs", self.t[:, None], self.resid, noise=self.err
                 )
@@ -279,24 +375,23 @@ class RVModel(Model):
                     f"gp_kernel must be None, or one of {KERNEL_LIST}"
                 )
 
-    def _get_synth_params(self):
-        """
-        Create vectors of parameters in "synth" basis to feed exoplanet orbit
-        model.
-        Stored as synth_dict attribute.
-        """
-        # NOTE: This will be good for all types of orbit models, not only RVs
-        synth_dict = dict()
-        nvars = self.named_vars
-        for pname in SYNTH_PARAMS:
-            synth_dict[pname] = pm.Deterministic(
-                pname,
-                tt.as_tensor_variable(
-                    [nvars[f"{prefix}_{pname}"] for prefix in self.planets]
-                ),
-            )
+    def _get_orbit(self):
 
-        self.synth_dict = synth_dict
+        # NOTE: will move to more generic model to support all timeseries.
+        # We access keys explicitely because the naming convention is
+        # different between RadVel and exoplanet.
+        # To use **, would need to "translate" radvel names to exoplanet or
+        # just use exoplanet names by default.
+
+        if len(self.planets) > 0:
+            return xo.orbits.KeplerianOrbit(
+                period=self.synth_dict["per"],
+                t_periastron=self.synth_dict["tp"],
+                ecc=self.synth_dict["e"],
+                omega=self.synth_dict["w"],
+            )
+        else:
+            return None
 
     def _get_rv_params(self):
         """
@@ -304,24 +399,41 @@ class RVModel(Model):
         RV calculation
         """
         nvars = self.named_vars
+        rv_params = dict()
 
         if "gamma" not in nvars:
-            pm.Deterministic("gamma", tt.as_tensor_variable(0.0))
+            rv_params["gamma"] = pm.Deterministic(
+                "gamma", tt.as_tensor_variable(0.0)
+            )
+        else:
+            rv_params["gamma"] = nvars["gamma"]
 
         if "dvdt" not in nvars:
-            pm.Deterministic("dvdt", tt.as_tensor_variable(0.0))
+            rv_params["dvdt"] = pm.Deterministic(
+                "dvdt", tt.as_tensor_variable(0.0)
+            )
+        else:
+            rv_params["dvdt"] = nvars["dvdt"]
 
         if "curv" not in nvars:
-            pm.Deterministic("curv", tt.as_tensor_variable(0.0))
+            rv_params["curv"] = pm.Deterministic(
+                "curv", tt.as_tensor_variable(0.0)
+            )
+        else:
+            rv_params["curv"] = nvars["curv"]
 
         if "wn" in nvars:
-            pass
+            rv_params["wn"] = nvars["wn"]
         elif "logwn" in nvars:
-            pm.Deterministic("wn", tt.exp(nvars["logwn"]))
+            rv_params["wn"] = pm.Deterministic("wn", tt.exp(nvars["logwn"]))
         else:
-            pm.Deterministic("wn", tt.as_tensor_variable(0.0))
+            rv_params["wn"] = pm.Deterministic(
+                "wn", tt.as_tensor_variable(0.0)
+            )
 
-    def get_rv_model(self, t: np.ndarray, name: str = "") -> pm.Distribution:
+        return rv_params
+
+    def calc_rv_model(self, t: np.ndarray, name: str = "") -> pm.Distribution:
         """
         Get the RV signal for the system.
 
@@ -330,18 +442,18 @@ class RVModel(Model):
         :param name: Name of the RV prediction (name of the variable will be
                      rv_model{_name}
         :type name: str, optional
-        :return: PyMC3 determinsitic variable representing the RV signal
+        :return: PyMC3 determinsitic variable rkepresenting the RV signal
         :rtype: pm.Distribution
         """
 
         suffix = "" if name == "" else f"_{name}"
 
         # Define the background RV model (constant at 0 if not provided)
+        rvpars = self.rv_params
         t_shift = t - self.t_ref
-        bkg = self.gamma
-        bkg += self.dvdt * t_shift
-        bkg += self.curv * t_shift ** 2
-        bkg = pm.Deterministic("bkg" + suffix, bkg)
+        bkg = rvpars["gamma"]
+        bkg += rvpars["dvdt"] * t_shift
+        bkg += rvpars["curv"] * t_shift ** 2
 
         if len(self.planets) > 0:
             # Calculate RV signal for each planet's orbit (2D array)
@@ -349,10 +461,11 @@ class RVModel(Model):
             pm.Deterministic("rv_orbits" + suffix, rvorb)
 
             # Sum over planets and add the background to get the full model
-            return pm.Deterministic(
-                "rv_model" + suffix, tt.sum(rvorb, axis=-1) + bkg
-            )
+            rv_model = tt.sum(rvorb, axis=-1) + bkg
         else:
-            return pm.Deterministic(
-                "rv_mode" + suffix, bkg
-            )
+            rv_model = bkg
+
+        # NOTE: This function may be called in unsuccseful add_likelihood
+        # So we don't set bkg and rv_model before the end to avoid model errors
+        pm.Deterministic("bkg" + suffix, bkg)
+        return pm.Deterministic("rv_model" + suffix, rv_model)
